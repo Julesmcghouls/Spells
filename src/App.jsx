0e470166786514ca649a4dbdd839feca8ba2c660
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-25 19:38:26 -0600

    Update App.jsx with new title and like button

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Title</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button> 
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <button onClick={() => setCount((count) => count + 1)}>
+          Like</button>
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This is a test!
       </p>
     </>
   )
