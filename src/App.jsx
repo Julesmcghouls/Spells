025df43fc0a5ee9eea26e3bdf895aea0caa4c295
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-27 23:31:31 -0600

    Add useEffect hook and display list of spells

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
+import {getAllSpells } from "./api"
 import './App.css'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
+  const [spells, setSpells] = useState([])
+
+  useEffect(() => {
+    getAllSpells().then(setSpells);
+  }, []);
 
   return (
     <>
-      <h1>Title</h1>
+      <h1>Spells</h1>
       <div className="card">
         <button> 
           count is {count}
@@ -19,8 +25,13 @@ function App() {
       <p className="read-the-docs">
         This is a test!
       </p>
+      <div className="App">
+      <ul>
+        {spells.map((spell) => (
+          <li key={spell.index}>{spell.name}</li>
+        ))}
+      </ul>
+    </div>
     </>
   )
 }
-
-export default App
